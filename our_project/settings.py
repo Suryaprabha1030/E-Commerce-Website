635fb1ef36e2d101c4b207f5cf5b0ba64a1145dd
Suryaprabha Chandrasekar <149596831+Suryaprabha1030@users.noreply.github.com>
2023-11-05 18:34:33 +0530

    Update settings.py

--- our_project/settings.py
+++ our_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
