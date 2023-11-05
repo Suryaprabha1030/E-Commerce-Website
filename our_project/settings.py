19e8d569297b4c7e648c57e25d9088cc32c50c2a
Suryaprabha Chandrasekar <149596831+Suryaprabha1030@users.noreply.github.com>
2023-11-05 18:52:41 +0530

    Update settings.py

--- our_project/settings.py
+++ our_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +84,7 @@ DATABASES = {
         'USER' :config('root'),
         'PASSWORD':config(''),
          'PORT':'3306',
+        'CONN_MAX_AGE': 300
     }
 }
 
